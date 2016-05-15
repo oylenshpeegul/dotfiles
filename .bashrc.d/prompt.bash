707968c0c0e4ebc7a06391be014212edf7f70009
oylenshpeegul <theaney@gmail.com>
2016-05-15 06:35:31 -0400

    fix whitepace

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -4,7 +4,7 @@ export PS1="\n${debian_chroot:+($debian_chroot)}\[\033[01;35m\]\u@\h\[\033[00m\]
 
 if [ $TERM = 'linux' -a $SHELL = '/bin/bash' ] ; then
 	# Turn off blinking cursor in virtual consoles
-    export PS1="\033[?17;0;127c$PS1"
+	export PS1="\033[?17;0;127c$PS1"
 else
 	# Set the window title in X terminals
 	export PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
