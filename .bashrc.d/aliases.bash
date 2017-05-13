f59a7198952e4cca35550952b1f3d37cba2eae88
oylenshpeegul <theaney@gmail.com>
2017-05-13 09:03:54 -0400

    Change clj alias.

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -4,6 +4,7 @@ alias E="EDITOR=\"emacsclient -c -a emacs\" sudoedit"
 
 alias burnit='cdrdao write --driver generic-mmc-raw cd.toc && rm *.wav'
 
-# start the Clojure REPL
-alias clj='java -cp ~/clojure/clojure-1.7.0/clojure-1.7.0.jar clojure.main'
-alias clojure=clj
+# # start the Clojure REPL
+# alias clj='java -cp ~/clojure/clojure-1.8.0/clojure-1.8.0.jar clojure.main'
+# alias clojure=clj
+# No, see $HOME/bin/clj instead
