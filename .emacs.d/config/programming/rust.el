7cc1deafba3187e24bffad249893e54e16952444
Tim Heaney <oylenshpeegul@pm.me>
2021-09-13 10:45:34 -0400

    Add lsp config

--- .emacs.d/config/programming/rust.el
+++ .emacs.d/config/programming/rust.el
@@ -1,1 +1,19 @@
 (use-package rustic)
+
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
